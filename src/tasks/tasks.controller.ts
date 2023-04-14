cb3741525cf6c7468257578bf5fa4c84dd244266
Daniel Chiwuzoh <chiwuzohdumebi@gmail.com>
2023-04-15 00:25:08 +0100

    Section 8 - Logging

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,7 +7,8 @@ import {
   Patch,
   Post,
   Query,
-  UseGuards
+  UseGuards,
+  Logger
 } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { CreateTaskDto } from './dto/create-task.dto'
@@ -21,6 +22,8 @@ import { GetUser } from 'src/auth/get-user.decorator'
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController')
+
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -28,6 +31,11 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retreiving all tasks. Filters: ${JSON.stringify(
+        filterDto
+      )}`
+    )
     return this.tasksService.getTasks(filterDto, user)
   }
 
@@ -41,6 +49,11 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(
+        createTaskDto
+      )}`
+    )
     return this.tasksService.createTask(createTaskDto, user)
   }
 
--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger
+} from '@nestjs/common'
 import {
   DataSource,
   // EntityRepository,
@@ -12,6 +16,8 @@ import { User } from 'src/auth/user.entity'
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository', { timestamp: true })
+
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager())
   }
@@ -32,9 +38,19 @@ export class TasksRepository extends Repository<Task> {
       )
     }
 
-    const tasks = await query.getMany()
+    try {
+      const tasks = await query.getMany()
 
-    return tasks
+      return tasks
+    } catch (err) {
+      this.logger.error(
+        `Failed to get tasks for user "${
+          user.username
+        }". Filters: ${JSON.stringify(filterDto)}`,
+        err.stack
+      )
+      throw new InternalServerErrorException()
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
