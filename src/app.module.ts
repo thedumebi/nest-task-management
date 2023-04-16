4ad71eabeb38c160d0383d26432637136c4dec32
Daniel Chiwuzoh <chiwuzohdumebi@gmail.com>
2023-04-16 01:05:30 +0100

    section 9 - configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,33 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { TasksModule } from './tasks/tasks.module'
 import { AuthModule } from './auth/auth.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { configValidationSchema } from './config.schema'
 
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ABcd12..',
-      database: 'task_management',
-      autoLoadEntities: true,
-      synchronize: true
-      // logging: true
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // what modules we depend on
+      inject: [ConfigService], // what do I need to inject into this module
+      useFactory: async (configService: ConfigService) => ({
+        // you must return. whatever you return would be what would be passed on
+        // the factory is called upon initialisation and the arrow fn can take anythig
+        // according to your injected stuff
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true
+        // logging: true
+      })
     }),
     AuthModule
   ]
